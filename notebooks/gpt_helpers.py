f38ce67987141a07eba5086e38c583414cf06355
slackroo <sumanthomgowda@gmail.com>
2025-08-26 18:13:51 +0530

    implement full GPT model with transformer blocks, LayerNorm, and text generation method

--- notebooks/gpt_helpers.py
+++ notebooks/gpt_helpers.py
@@ -97,3 +97,127 @@ class MultiHeadAttention(nn.Module):
         context_vec = self.out_proj(context_vec)  # optional projection
 
         return context_vec
+
+class LayerNorm(nn.Module):
+    def __init__(self, emb_dim):
+        super().__init__()
+        self.eps = 1e-5
+        self.scale = nn.Parameter(torch.ones(emb_dim))
+        self.shift = nn.Parameter(torch.zeros(emb_dim))
+
+    def forward(self, x):
+        mean = x.mean(dim=-1, keepdim=True)
+        var = x.var(dim=-1, keepdim=True, unbiased=False)  # unbiased true is population stats and False is sample stats
+        norm_x = (x - mean) / torch.sqrt(var + self.eps)
+        return self.scale * norm_x + self.shift
+
+
+class GELU(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        return 0.5 * x * (1 + torch.tanh(
+            torch.sqrt(torch.tensor(2.0 / torch.pi)) *
+            (x + 0.044715 * torch.pow(x, 3))
+        ))
+
+class FeedForward(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.layers = nn.Sequential(
+            nn.Linear(cfg['emb_dim'], 4 * cfg['emb_dim']),
+            GELU(),
+            nn.Linear(4 * cfg['emb_dim'], cfg['emb_dim']),
+        )
+
+    def forward(self, x):
+        return self.layers(x)
+
+
+class TransformerBlock(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.attention = MultiHeadAttention(
+            d_in=cfg['emb_dim'],
+            d_out=cfg['emb_dim'],
+            context_length=cfg['context_length'],
+            num_heads=cfg['n_heads'],
+            dropout=cfg['drop_rate'],
+            qkv_bias=cfg['qkv_bias'],
+        )
+        self.ff = FeedForward(cfg)
+        self.norm1 = LayerNorm(cfg['emb_dim'])
+        self.norm2 = LayerNorm(cfg['emb_dim'])
+        self.drop_shortcut = nn.Dropout(cfg['drop_rate'])
+
+    def forward(self, x):
+        shortcut = x
+        x = self.norm1(x)
+        x = self.attention(x) # Shape (batch_size, num_tokens, emb_dim)
+        x = self.drop_shortcut(x)
+        x = x + shortcut
+
+        # shortcut connection for feed forward block
+        shortcut = x
+        x = self.norm2(x)
+        x = self.ff(x)
+        x = self.drop_shortcut(x)
+        x = x + shortcut
+
+        return x
+
+class GPTModel(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.tok_emb = nn.Embedding(cfg['vocab_size'], cfg['emb_dim'])
+        self.pos_emb = nn.Embedding(cfg['context_length'], cfg['emb_dim'])
+        self.drop_emb = nn.Dropout(cfg['drop_rate'])
+
+        # placeholder for the transformer block
+        self.trf_block = nn.Sequential(
+            *[TransformerBlock(cfg) for _ in range(cfg['n_layers'])]
+        )
+        # use a placeholder for layer norm
+        self.final_norm = LayerNorm(cfg['emb_dim'])
+        self.out_head = nn.Linear(
+            cfg['emb_dim'], cfg['vocab_size'], bias=False
+        )
+
+    def forward(self, x):
+        batch_size, seq_len = x.shape
+        tok_embeds = self.tok_emb(x)
+        pos_embeds = self.pos_emb(torch.arange(seq_len, device=x.device))
+        x = tok_embeds + pos_embeds
+        x = self.drop_emb(x)
+        x = self.trf_block(x)
+        x = self.final_norm(x)
+        logits = self.out_head(x)
+        return logits
+
+
+def generate_text_simple(model, idx, max_new_tokens=100, context_size=1024):
+    # idx is (Batch, Token) array of indices in the current context
+    for _ in range(max_new_tokens):
+
+        # Crop current context if it exceeds the context size limit
+        # E.g., if LLM supports only 5 tokens, and the context size is 10
+        # then the last 5 tokenes are used
+        idx_cond = idx[:, -context_size:]
+
+        # get the predictions
+        with torch.no_grad():
+            logits = model(idx_cond)
+
+        # Focus only on the last time step
+        # (batch , n_tokens, vocab_size) becomes (batch, vocab_size)
+        logits = logits[:, -1, :]
+        probas = torch.softmax(logits, dim=-1)
+
+        # get the idx of the vocab enty with the highest logits value
+        idx_next = torch.argmax(probas, dim=-1, keepdim=True)
+
+        # append sampled index to the running sequence
+        idx = torch.cat((idx, idx_next), dim=-1)
+
+    return idx
